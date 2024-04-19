bcb8d0aad3644a8dba25adc7b8d90b4ba31b8748
art1544 <141589021+art1544@users.noreply.github.com>
2024-04-19 01:36:44 -0300

    Att dependencies

---
+++ testedti/src/main/java/com/testedti/petshopfinder/testedti/TestedtiApplication.java
@@ -0,0 +1,13 @@
+package com.testedti.petshopfinder.testedti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestedtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestedtiApplication.class, args);
+	}
+
+}
---
+++ testedti/src/test/java/com/testedti/petshopfinder/testedti/TestedtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testedti.petshopfinder.testedti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestedtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
